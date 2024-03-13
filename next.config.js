29c50a37d51e48c47673ea5854c1deb917fe3f9b
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2024-03-13 02:30:31 +0000

    feat(invoices): add update dan delete features

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: [
+        'localhost',
+        'friendly-couscous-76q6vqrjq4p3rvp9-3000.app.github.dev',
+      ],
+      allowedOrigins: [
+        'http://localhost',
+        'https://friendly-couscous-76q6vqrjq4p3rvp9-3000.app.github.dev',
+      ],
+    },
+  },
+};
 
 module.exports = nextConfig;
